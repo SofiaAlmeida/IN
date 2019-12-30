e4624c03cfd0d06642527d124d01e153481f5548
Francisco Almeida <falmeida@ull.es>
2019-12-30 08:51:32 +0000

    Stacking sin selección de variables p3_24

--- P3/src/p3_24.py
+++ P3/src/p3_24.py
@@ -109,11 +109,11 @@ skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=123456)
 print("------ Stacking...")
 
 estimators = [
-    ('lgbm', lgb.LGBMClassifier(objective='regression_l1', n_jobs = -1. n_estimators=1000, num_leaves = 80, scale_pos_weight = 0.05, verbose = 2)),
+    ('lgbm', lgb.LGBMClassifier(objective='regression_l1', n_jobs = -1, n_estimators=1000, num_leaves = 80, scale_pos_weight = 0.05, verbose = 2)),
     ('rf', RandomForestClassifier(random_state = 123456, n_jobs = -1, max_depth = 30, n_estimators = 400, verbose = 2)),
     ('xgboost', xgb.XGBClassifier(predictor='cpu_predictor', n_gpus=0, n_jobs = -1, n_estimators = 700, eta = 0.1, max_depth = 10, verbose=2))]
 
-stacking = StackingClassifier(estimators = estimators, final_estimator = LogisticRegression(), n_jobs = -1, cv = 3, verbose = 2)
+stacking = StackingClassifier(estimators = estimators, final_estimator = LogisticRegression(), cv = 5, verbose = 2)
 
 #stacking, y_test_stacking = validacion_cruzada(stacking, X, y, skf)
 
